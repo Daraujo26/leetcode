80ad51aa63f62d82a56f9fe1a4a09facba8bc71d
Daraujo26 <David.araujo2130@gmail.com>
2024-02-27 14:45:48 -0500

    Adding additional comments

--- python/twoSum.py
+++ python/twoSum.py
@@ -1,4 +1,4 @@
-# 1. Two Sum
+# 1. Two Sum (Easy)
 # Given an array of integers nums and an integer target, 
 # return indices of the two numbers such that they add up to target.
 
@@ -10,6 +10,6 @@ def twoSum(nums, target):
             elif nums[i] + nums[j] == target:
                 return [i, j]
 
-
+# This is the O(n^2) solution, there is a faster O(nlog(n)) solutions as well
 
 print(twoSum([2,7,11,15], 26))
